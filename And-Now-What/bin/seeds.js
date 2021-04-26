9cfb9ebdce2629090f1dc170fd90e11161a621c5
jguevaraa <juniorguevaraa@hotmail.com>
2021-04-26 12:54:32 +0200

    seeds

--- And-Now-What/bin/seeds.js
+++ And-Now-What/bin/seeds.js
@@ -1,19 +1,71 @@
 const mongoose = require('mongoose')
-// Call the Celebrity model's create method with the array as argument.
-const Celebrity = require('../models/celebrity.model')
 
+const Ingredent = require('../models/ingredent.model')
 
-const spoonacularAPI = new SpotifyWebApi({
-  clientId: process.env.CLIENT_ID,
-  clientSecret: process.env.CLIENT_SECRET,
-});
+mongoose.connect('mongodb://localhost/and_now_what')
 
-fca5115684f049309c7a9cf8dd175018
+const ingredents = [
+  {
+    ingrendent: "banana",
+    title: "Xocai Healthy Chocolate Peanut Butter Bannana Dip",
+    image: "https://spoonacular.com/recipeImages/665469-312x231.jpg",
+    likes: 1,
+    },
+  {
+    ingrendent: "banana",
+    title: "Luscious Orange Cardamom Smoothie",
+    image: "https://spoonacular.com/recipeImages/650485-312x231.jpg",
+    likes: 1,
+    },
 
-// Retrieve an access token
-spotifyApi
-  .clientCredentialsGrant()
-  .then((data) => spotifyApi.setAccessToken(data.body["access_token"]))
-  .catch((error) =>
-    console.log("Something went wrong when retrieving an access token", error)
-  );
+    {
+      ingrendent: "banana",
+      title: "Peanut Butter and Jelly Smoothie",
+      image: "https://spoonacular.com/recipeImages/655235-312x231.jpg",
+      likes: 58,
+      },
+    {
+      ingrendent: "tuna",
+      title: "Tuna Stuffed Hard-Boiled Eggs",
+      image: "https://spoonacular.com/recipeImages/663950-312x231.jpg",
+      likes: 4
+    },
+    {
+      ingrendent: "tuna",
+      title: "Baharat Panko Crusted Albacore Tuna",
+      image: "https://spoonacular.com/recipeImages/633386-312x231.jpg",
+      likes: 3
+    },
+    {
+      ingrendent: "meat",
+      title: "Instant Pot Hawaiian Chicken",
+      image: "https://spoonacular.com/recipeImages/982365-312x231.jpg",
+      likes: 19
+    },
+    {
+      ingrendent: "meat",
+      title: "Bacon Ice Cream",
+      image: "https://spoonacular.com/recipeImages/633314-312x231.jpg",
+      likes: 9
+    },
+    {
+      ingrendent: "onion",
+      title: "Caramelized Onion Dip",
+      image: "https://spoonacular.com/recipeImages/716422-312x231.jpg",
+      likes: 95
+    },
+    {
+      ingrendent: "onion",
+      title: "Glazed Pearl Onions",
+      image: "https://spoonacular.com/recipeImages/644758-312x231.jpg",
+      likes: 1
+    },
+]
+
+Ingredent
+    .create(ingredents)
+    .then(data => {
+        console.log(data)
+        mongoose.connection.close()
+    })
+    .catch(err => console.error(err))
